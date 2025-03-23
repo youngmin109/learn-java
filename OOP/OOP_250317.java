9634cceb4ba57bfbba94bfbd42de80996571d54a
youngmin109 <qhqo3325@gmail.com>
2025-03-23 20:21:39 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

--- OOP/OOP_250317.java
+++ OOP/OOP_250317.java
@@ -1,14 +1,14 @@
 package OOP;
 
 class Car {
-    String brand;
+    static String brand;
     int speed;
 
-    // 기본 생성자
-    Car() {
-        this.brand = "Unknown";
-        this.speed = 0;
-    }
+//    // 기본 생성자
+//    Car() {
+//        this.brand = "Unknown";
+//        this.speed = 100;
+//    }
 
     // 브랜드만 설정하는 생성자
     Car (String brand) {
@@ -22,6 +22,12 @@ class Car {
         this.brand = brand;
         this.speed = speed;
         System.out.println("브랜드: " + brand + ", 속도: " + speed + "km/h");
+
+    }
+    static void Inter() {
+        String brand = "3dd";
+        int speed = 3;
+        System.out.println("sut");
     }
 }
 
@@ -33,5 +39,8 @@ public class OOP_250317 {
         Car car2 = new Car("Hyundai"); // 브랜드만 지정
         Car car3 = new Car("BMW", 120); // 브랜드와 속도 지정
 
+        Car.Inter();
+        Car.brand = "hello";
+        car1.speed = 110;
     }
 }
